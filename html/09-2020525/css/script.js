d3789a564ce50e2ff58f830fe3ba6d4abac20c93
thoai0412 <thoai0412@users.noreply.github.com>
2020-05-27 17:57:48 +0800

    lo

---
+++ html/09-2020525/css/script.js
@@ -0,0 +1,16 @@
+function turnbat(){
+    var img= document.getElementById("anh")
+    img.src ="./images/pic_bulbon.gif";
+}
+function turntat(){
+    var img= document.getElementById("anh")
+    img.src ="./images/pic_bulboff.gif";
+}
+function ngoctrinh(){
+    console.log('vu to');
+    console.log('mong to');
+
+}
+function giacombotinhban(){
+    document.write('500k mot shot')
+}
